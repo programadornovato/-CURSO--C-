9b26c4043310699fd357a35c40cbaa168492ef06
Programador Novato <info@programadornovato.com>
2021-04-24 17:57:30 -0500

    Comentarios en C# [05]
    Video:
    https://youtu.be/OMh8jSv5n-c

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-
+/*
+Autor: Programador novato
+Fecha: 01/01/2021
+Este programa hace que los novatos aprenden a hacer
+un codigo bonito y ya
+*/
 namespace HolaMundo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Cambiamos el color del texto a amarillo y del fondo a azul
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.Clear();
+            //escribimos texto y esperamos a que se precione una tecla
             Console.WriteLine("Hola mundo :)");
-            Console.ForegroundColor = ConsoleColor.Green;
+            //Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Hola Novato :)");
             Console.Read();
         }
