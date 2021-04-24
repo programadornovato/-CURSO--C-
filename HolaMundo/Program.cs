0481a69e71dfbe288dafdd4fea8e8008394cfba4
Programador Novato <info@programadornovato.com>
2021-04-24 16:42:49 -0500

    Colores de consola en C# [04]
    Video:
    https://youtu.be/Y3rxV1JT8Gw

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -6,7 +6,12 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
             Console.WriteLine("Hola mundo :)");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hola Novato :)");
             Console.Read();
         }
     }
