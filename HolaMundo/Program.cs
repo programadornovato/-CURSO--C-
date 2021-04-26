91a56a92d8b9e3141df071d5f87a933ed3e13e9d
Programador Novato <info@programadornovato.com>
2021-04-26 03:23:09 -0500

    Tipos de dato string en C# [08]
    Video:
    https://youtu.be/J1lSAs1PalM

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -11,20 +11,10 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            int num1 = 6;
-            int num2 = 7;
-            int suma = num1 + num2;
-            int num3 = 3, num4 = 4, num5 = 15;
-            double numd1 = 5.5;
-            double numd2 = 6.6;
-            double sumad = numd1 + numd2;
-            float numFloat = 5.5f;
-            //float sumaf = numFloat + numd1;
-            Console.WriteLine("El valor de num1=" + num1 + " el tipo es=" + num1.GetType());
-            Console.WriteLine("El valor de suma=" + suma + " el tipo es=" + suma.GetType());
-            Console.WriteLine("El valor de sumad=" + sumad + " el tipo es=" + sumad.GetType());
-            Console.WriteLine("El valor de numFloat=" + numFloat + " el tipo es=" + numFloat.GetType());
-
+            string miPagina = "ProgramadorNovato.com";
+            string presentacion = "Mi pagina web es: ";
+            miPagina = miPagina.ToLower();
+            Console.WriteLine(presentacion+ miPagina);
             Console.Read();
         }
     }
