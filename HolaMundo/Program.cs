e4971e56700da6bbf215522a4e789bc1a5d0664f
Programador Novato <info@programadornovato.com>
2021-04-26 01:38:41 -0500

    (Ejercicio) Tipos de datos en C# [07]
    Video:
    https://youtu.be/Fv4A6giccB0

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -11,14 +11,20 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            //Cambiamos el color del texto a amarillo y del fondo a azul
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.Clear();
-            //escribimos texto y esperamos a que se precione una tecla
-            Console.WriteLine("Hola mundo :)");
-            //Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Hola Novato :)");
+            int num1 = 6;
+            int num2 = 7;
+            int suma = num1 + num2;
+            int num3 = 3, num4 = 4, num5 = 15;
+            double numd1 = 5.5;
+            double numd2 = 6.6;
+            double sumad = numd1 + numd2;
+            float numFloat = 5.5f;
+            //float sumaf = numFloat + numd1;
+            Console.WriteLine("El valor de num1=" + num1 + " el tipo es=" + num1.GetType());
+            Console.WriteLine("El valor de suma=" + suma + " el tipo es=" + suma.GetType());
+            Console.WriteLine("El valor de sumad=" + sumad + " el tipo es=" + sumad.GetType());
+            Console.WriteLine("El valor de numFloat=" + numFloat + " el tipo es=" + numFloat.GetType());
+
             Console.Read();
         }
     }
