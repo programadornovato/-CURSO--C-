8f01c34a281fb1a10ebd7495fc48482675bd5e30
Programador Novato <info@programadornovato.com>
2021-04-26 05:51:24 -0500

    Parse (Cambio) de tipos de datos en C# [09]
    Video:
    https://youtu.be/sOLtmnXPOWE

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -11,10 +11,26 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            string miPagina = "ProgramadorNovato.com";
-            string presentacion = "Mi pagina web es: ";
-            miPagina = miPagina.ToLower();
-            Console.WriteLine(presentacion+ miPagina);
+            string numString1 = "15.55";
+            string numString2 = "25.22";
+            /*
+            //string resultadoS = numString1+ numString2;
+            int num1 = Int32.Parse(numString1);
+            int num2 = Int32.Parse(numString2);
+            int resultadoI = num1 + num2;
+            Console.WriteLine(resultadoI);
+            */
+            /*
+            float numFloat1 = float.Parse(numString1);
+            float numFloat2 = float.Parse(numString2);
+            float resultadoF = numFloat1 + numFloat2;
+            Console.WriteLine(resultadoF);
+            */
+            double numDoble1 = double.Parse(numString1);
+            double numDoble2 = double.Parse(numString2);
+            double resultadoD = numDoble1 + numDoble2;
+            Console.WriteLine(resultadoD);
+            
             Console.Read();
         }
     }
