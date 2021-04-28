8ab8626c282eb81265da18f8c0e64815f18a8f29
Programador Novato <info@programadornovato.com>
2021-04-27 21:42:09 -0500

    Clase matemáticas (funciones matemáticas) en C# [17]
    Video:
    https://youtu.be/EDrMo4QIoXY

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -11,20 +11,20 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            //int años=edad(1980);
-            Console.WriteLine("Edad es "+ edad(1980));
-            Console.WriteLine("El año de nacimiento es "+ añoNacimiento(20));
+            Console.WriteLine("Math.PI=" + Math.PI);
+            Console.WriteLine("Math.E=" + Math.E);
+            Console.WriteLine("Math.Min(5.5,3.5)=" + Math.Min(5.5,3.5));
+            Console.WriteLine("Math.Max(5.5,3.5)=" + Math.Max(5.5,3.5));
+            Console.WriteLine("Math.Round(-5.5)=" + Math.Round(-5.5));
+            Console.WriteLine("Math.Ceiling(-5.5)=" + Math.Ceiling(-5.5));
+            Console.WriteLine("Math.Floor(5.5)=" + Math.Floor(5.5));
+            Console.WriteLine("Math.Truncate(-5.5)=" + Math.Truncate(-5.5));
+            Console.WriteLine("Math.Abs(5.5)=" + Math.Abs(5.5));
+            Console.WriteLine("Math.Sign(5.5)=" + Math.Sign(5.5));
+            Console.WriteLine("Math.Pow(5,3)=" + Math.Pow(5,3));
+            Console.WriteLine("Math.Sqrt(9)=" + Math.Sqrt(9));
+
             Console.Read();
-        }
-        public static int edad(int añoNacimiento) {
-            int añosActual = DateTime.Now.Year;
-            //int edad = añosActual - añoNacimiento;
-            return añosActual - añoNacimiento;
-        }
-        public static int añoNacimiento(int edad) {
-            int añoActual = DateTime.Now.Year;
-            //int añoNacimiento = añoActual - edad;
-            return añoActual - edad;
         }
     }
 }
