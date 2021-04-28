05f5e3f21b78aad6496f39a20794fd0055bcc564
Programador Novato <info@programadornovato.com>
2021-04-28 03:45:57 -0500

    Condicionales en C# (if, else if y else)[19]
    Video:
    https://youtu.be/5uLJKfwsmMI

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -11,14 +11,25 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("¿Humano como te llamas?");
-            string nombre = Console.ReadLine();
-            Console.WriteLine("¿"+ nombre +" en que año naciste?");
-            int añoNacimiento = int.Parse(Console.ReadLine());
-            int edad = DateTime.Now.Year - añoNacimiento;
-            Console.WriteLine("¿" + nombre + " de pura casualidad cunto ganas al mes?");
-            float salario = float.Parse(Console.ReadLine());
-            Console.WriteLine("Pobre "+nombre+" a tus "+edad+" años y solo ganas "+salario+ " pesitos ");
+            Console.WriteLine("Humano ingresa un NUMERO entre 0 y 10:");
+            double calificacion = double.Parse(Console.ReadLine());
+            int calRedondeada = Convert.ToInt32(Math.Round(calificacion));
+            if ((calRedondeada >= 0) && (calRedondeada < 6))
+            {
+                Console.WriteLine("El alumno se chingo");
+            }
+            else if ((calRedondeada >= 6) && (calRedondeada <= 9))
+            {
+                Console.WriteLine("El alumno ya chingo");
+            }
+            else if (calRedondeada == 10)
+            {
+                Console.WriteLine("El alumno es una v3rg4");
+            }
+            else {
+                Console.WriteLine("Humano estupido te pedi un numero entre 0 y 10");
+            }
+
             Console.Read();
         }
     }
