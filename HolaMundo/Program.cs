9f26f7b726ad68b342148d70bc96f79036a18c64
Programador Novato <info@programadornovato.com>
2021-05-08 17:37:40 -0500

    Operadores de incremento y decremento en C#[25]#️⃣
    Video:
    https://youtu.be/VVyLu3hUwrk

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -11,31 +11,18 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            double a = 6;
-            double b = 5;
-            Console.WriteLine("a="+a+" b="+b);
+            int a = 1;
+            int b = 2;
+            //a =a + 1;
+            //a +=1;
             /*
-            //a = a + b;
-            a += b;
-            Console.WriteLine("a=" + a + " b=" + b);
-            
-            //a = a - b;
-            a -= b;
-            Console.WriteLine("a=" + a + " b=" + b);
-
-            //a = a * b;
-            a *= b;
-            Console.WriteLine("a=" + a + " b=" + b);
-
-            //a = a / b;
-            a /= b;
-            Console.WriteLine("a=" + a + " b=" + b);
+            a++;
+            Console.WriteLine("a="+a+" b="+b);
+            b--;
+            Console.WriteLine("a="+a+" b="+b);
             */
-            //a = a % b;
-            a %= b;
+            b = ++a;
             Console.WriteLine("a=" + a + " b=" + b);
-
-
             Console.Read();
         }
     }
