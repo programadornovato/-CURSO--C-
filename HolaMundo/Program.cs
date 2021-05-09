d91449c164a7a484c9921d1dd95c39893f2923e4
Programador Novato <info@programadornovato.com>
2021-05-09 07:54:47 -0500

    Ejercicio 05.- Redondear el cambio en C#[26]
    Video:
    https://youtu.be/XjRuehDFV40

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -11,18 +11,32 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            int a = 1;
-            int b = 2;
-            //a =a + 1;
-            //a +=1;
-            /*
-            a++;
-            Console.WriteLine("a="+a+" b="+b);
-            b--;
-            Console.WriteLine("a="+a+" b="+b);
-            */
-            b = ++a;
-            Console.WriteLine("a=" + a + " b=" + b);
+            try
+            {
+                Console.WriteLine("Bienvenido a su tienda PN");
+                Console.WriteLine("Ingresa el valor del producto 1:");
+                double p1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa el valor del producto 2:");
+                double p2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa el valor del producto 3:");
+                double p3 = double.Parse(Console.ReadLine());
+                double suma = p1 + p2 + p3;
+                Console.WriteLine("Humano deseas redondear tus centavos S/N");
+                string respuesta = Console.ReadLine();
+                if ((respuesta == "S") || (respuesta == "s"))
+                {
+                    double sumaRedondeada = Math.Ceiling(suma);
+                    double redendeo = sumaRedondeada - suma;
+                    Console.WriteLine("Gracias humano por haber redondeado " + String.Format("{0:C}", redendeo));
+                }
+                else
+                {
+                    Console.WriteLine("Pinche humano miserable");
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine("Pinche cajero tonto escribiste algo mal "+ e);
+            }
             Console.Read();
         }
     }
