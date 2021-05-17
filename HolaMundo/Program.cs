9801f2c51c160acbc40b7288347a0d3e50f965c8
Programador Novato <info@programadornovato.com>
2021-05-17 16:06:17 -0500

    Matriz Transpuesta en C#[55]#️⃣
    Video:
    https://youtu.be/WKF3NCksyM4

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -13,32 +13,41 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            string[,] alumnos = new string[7,2];
-            for (int i = 0; i < alumnos.GetLength(0); i++)
+            int[,] matrizO = new int[3,3];
+            int[,] matrizT = new int[3,3];
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine("Humano ingresa el nombre del alumno "+(i+1));
-                alumnos[i,0]=Console.ReadLine();
-                Console.WriteLine("Humano ingresa el salon del alumno "+(i+1));
-                alumnos[i,1]=Console.ReadLine();
-            }
-            Console.WriteLine("Humano ingresa el salon de los alumnos que quieres ver ");
-            string salon = Console.ReadLine();
-            for (int i = 0; i < alumnos.GetLength(0); i++)
-            {
-                if (alumnos[i, 1] == salon) {
-                    /*
-                    Console.Write(alumnos[i,0]);
-                    Console.Write(" | "+alumnos[i,1]);
-                    */
-                    for (int j = 0; j < alumnos.GetLength(1); j++)
-                    {
-                        Console.Write(alumnos[i,j]+" | ");
-                    }
-
+                for (int j = 0; j < 3; j++)
+                {
+                    matrizO[i, j] = int.Parse(Interaction.InputBox("Humano ingresa el elemento de la matrizO["+(i+1)+ "," + (j + 1) + "]"));
                 }
-                Console.WriteLine();
             }
-            Console.Read();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrizT[i, j] = matrizO[j, i];
+                }
+            }
+            string resultado = "Matriz original\n";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    resultado = resultado + matrizO[i, j] + " | ";
+                }
+                resultado = resultado + "\n";
+            }
+            resultado = resultado+"\n\nMatriz original\n";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    resultado = resultado + matrizT[i, j] + " | ";
+                }
+                resultado = resultado + "\n";
+            }
+            MessageBox.Show(resultado);
         }
     }
 }
