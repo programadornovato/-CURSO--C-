e115ff520baea0d8dbe660d850e8208556558a28
Programador Novato <info@programadornovato.com>
2021-05-17 04:11:45 -0500

    Búsqueda Binaria en C#[52]#️⃣
    Video:
    https://youtu.be/OhNYiR2zFn0

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -13,30 +13,40 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Humano ingresa el numero a buscar de la lista");
-            int numeroBuscar=int.Parse(Console.ReadLine());
+            Console.WriteLine("Humano ingresa el numero a buscar");
+            int numeroBuscar = int.Parse(Console.ReadLine());
             int[] listaNumeros = new int[7];
             for (int i = 0; i < 7; i++)
             {
-                Console.WriteLine("Humano ingresa el elemento "+(i+1)+" de la lista");
-                int numero = int.Parse(Console.ReadLine());
-                listaNumeros[i] = numero;
+                Console.WriteLine("Huamano ingresa el valor del elemento "+(i+1));
+                listaNumeros[i]=int.Parse(Console.ReadLine());
             }
-            int pos = 0;
+            int n = listaNumeros.Length;
+            int inf = 0;
+            int centro = 0;
+            int sup = n - 1;
             bool bandera = false;
-            while (pos < 7 && bandera == false) {
-                if (numeroBuscar == listaNumeros[pos]) {
+            while (inf <= sup) {
+                centro = (sup + inf) / 2;
+                if (listaNumeros[centro] == numeroBuscar)
+                {
                     bandera = true;
                     break;
                 }
-                pos++;
+                else if (numeroBuscar < listaNumeros[centro])
+                {
+                    sup = centro - 1;
+                }
+                else {
+                    inf = centro + 1;
+                }
             }
             if (bandera == true)
             {
-                Console.WriteLine("Felicidades humano se encontro el numero " + numeroBuscar + " en la posicion " + (pos+1));
+                Console.WriteLine("Felicidades humano el numero " + numeroBuscar + " estaba en la posicion " + (centro + 1));
             }
             else {
-                Console.WriteLine("Lo siento humano no se encontro el numero "+numeroBuscar);
+                Console.WriteLine("Humano estupido el numero "+numeroBuscar+" no se encuentra en el arreglo");
             }
             Console.Read();
         }
