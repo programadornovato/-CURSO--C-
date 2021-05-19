dd2a3872542056bf8058bbf8d7de3e58854ea084
Programador Novato <info@programadornovato.com>
2021-05-19 07:19:03 -0500

    Ejemplo de POO (Programación Orientada a Objetos) en C#[60]#️⃣
    Video:
    https://youtu.be/_LIY5nlWH24

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -13,54 +13,42 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            int[,] matriz = new int[5, 5];
-            int dato = 1;
-            for (int i = 0; i < matriz.GetLength(0); i++)
-            {
-                for (int j = 0; j < matriz.GetLength(1); j++)
-                {
-                    matriz[i, j] = dato;
-                    dato++;
-                }
-            }
-            for (int i = 0; i < matriz.GetLength(0); i++)
-            {
-                for (int j = 0; j < matriz.GetLength(1); j++)
-                {
-                    Console.Write(matriz[i, j]+"\t");
-                }
-                Console.WriteLine();
-            }
-            
-            int[] diagonalPrincipal = new int[matriz.GetLength(0)];
-            int[] diagonalSecundaria = new int[matriz.GetLength(0)];
-            for (int i = 0; i < matriz.GetLength(0); i++)
-            {
-                for (int j = 0; j < matriz.GetLength(1); j++)
-                {
-                    if (i == j) {
-                        diagonalPrincipal[i] = matriz[i, j];
-                    }
-                    if ((i + j) == (matriz.GetLength(0) - 1)) {
-                        diagonalSecundaria[i] = matriz[i, j];
-                    }
-                }
-            }
-            Console.WriteLine();
-            int suma = 0;
-            foreach (int numero in diagonalPrincipal) {
-                Console.Write(numero+"\t");
-                suma = suma + numero;
-            }
-            Console.WriteLine("suma diagonal princiapal="+suma+"\n");
-            suma = 0;
-            foreach (int numero in diagonalSecundaria)
-            {
-                Console.Write(numero + "\t");
-                suma = suma + numero;
-            }
-            Console.WriteLine("suma diagonal secundaria=" + suma + "\n");
+            Auto vocho1 = new Auto();
+            vocho1.color = "Verde";
+            vocho1.modelo = 1980;
+            vocho1.marca = "Vocho";
+            Console.WriteLine(vocho1.color);
+            Console.WriteLine(vocho1.modelo);
+            Console.WriteLine(vocho1.marca);
+            vocho1.enciende();
+            vocho1.acelera();
+            vocho1.frena();
+
+            Auto chevy = new Auto();
+            chevy.color = "azul";
+            chevy.modelo = 2010;
+            chevy.marca = "Chevrolet";
+            Console.WriteLine(chevy.color);
+            Console.WriteLine(chevy.modelo);
+            Console.WriteLine(chevy.marca);
+            chevy.enciende();
+            chevy.acelera();
+            chevy.frena();
             Console.Read();
+        }
+    }
+    class Auto {
+        public string color = "";
+        public int modelo = 0;
+        public string marca = "";
+        public void enciende() {
+            Console.WriteLine("Se encendio el auto run run");
+        }
+        public void acelera() {
+            Console.WriteLine("El auto esta acelerando");
+        }
+        public void frena() {
+            Console.WriteLine("El auto ha frenado");
         }
     }
 }
