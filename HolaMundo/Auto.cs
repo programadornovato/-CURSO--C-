3667aba8db3550a57c50c8697a8a54a6e69fdf0d
Programador Novato <info@programadornovato.com>
2021-05-20 08:41:13 -0500

    Llamar nuestras clases desde otros archivos en C#[62]#️⃣
    Video:
    https://youtu.be/_Dss-Nn_d_E

---
+++ HolaMundo/Auto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+class Auto
+{
+    private string color = "";
+    private int modelo = 0;
+    private string marca = "";
+    private bool acceso = false;
+    public void meterLlave(string llave)
+    {
+        if (llave == "123456")
+        {
+            acceso = true;
+            Console.WriteLine("Se tien acceso al auto");
+        }
+        else
+        {
+            acceso = false;
+            Console.WriteLine("Llave incorrecta");
+        }
+    }
+    public void mando(string accion)
+    {
+        if (acceso == true)
+        {
+            if (accion == "enciende")
+            {
+                enciende();
+            }
+            if (accion == "acelera")
+            {
+                acelera();
+            }
+
+            if (accion == "frena")
+            {
+                frena();
+            }
+        }
+        else
+        {
+            Console.WriteLine("No tienes acceso al auto");
+        }
+
+    }
+    private void enciende()
+    {
+        Console.WriteLine("Se encendio el auto run run");
+    }
+    private void acelera()
+    {
+        Console.WriteLine("El auto esta acelerando");
+    }
+    private void frena()
+    {
+        Console.WriteLine("El auto ha frenado");
+    }
+}
